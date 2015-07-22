cbc8c4c5e67deb8f2de0d148f0d03b4fe1f37a38
laipho94 <lai.pho3fb@gmail.com>
2015-07-22 19:03:26 +0700

    Up dự án

---
+++ TinTucBongDa/TinTucBongDa/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TinTucBongDa.MvcApplication" Language="C#" %>
---
+++ TinTucBongDa/TinTucBongDa/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TinTucBongDa.Startup))]
+namespace TinTucBongDa
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
